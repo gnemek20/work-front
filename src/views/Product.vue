cbd71bf70e028d4527b9b0e1ff5813eddb2301ec
gnemek20 <gnemek20@gmail.com>
2023-12-02 00:54:50 +0900

    상품 진열 형태만 잡아둠

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -13,6 +13,18 @@
           <h4>상품을 검색하거나 표시될 상품의 조건을 설정할 수 있는 창</h4>
         </div>
       </div>
+      <div class="product-content">
+        <div class="title">
+          <h2>태그</h2>
+          <p>설명</p>
+          <hr>
+        </div>
+        <div class="flex justify-center">
+          <div class="list">
+            <p v-for="count in 10" v-bind:key="count">{{ count }}</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,6 +46,7 @@ export default {
 <style lang="scss" scoped>
 .cover {
   display: flex;
+  flex-direction: column;
   width: 100%;
   height: 400px;
   position: relative;
@@ -72,6 +85,32 @@ export default {
       height: calc(250px - 40px);
       padding: 20px;
       background-color: rgba(#f5f5f5, 0.8);
+    }
+  }
+  .product-content {
+    margin-top: 100px;
+
+    .title {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 5px;
+
+      hr {
+        width: 50px;
+        border: 0;
+        border-top: 1px solid dimgray !important;
+      }
+    }
+    .list {
+      justify-content: center;
+      width: 100%;
+      max-width: 1400px;
+      margin-top: 100px;
+      display: grid;
+      grid-template-columns: repeat(auto-fit, 252px);
+      grid-auto-rows: 352px;
+      gap: 25px;
     }
   }
 }
